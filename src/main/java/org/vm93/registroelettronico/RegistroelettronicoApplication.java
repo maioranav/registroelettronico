1502a7e48846c99736ab6847691466a7feb9be23
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-06 12:08:12 +0200

    initial commit with dependencies

---
+++ src/main/java/org/vm93/registroelettronico/RegistroelettronicoApplication.java
@@ -0,0 +1,13 @@
+package org.vm93.registroelettronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroelettronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroelettronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vm93/registroelettronico/RegistroelettronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vm93.registroelettronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroelettronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
