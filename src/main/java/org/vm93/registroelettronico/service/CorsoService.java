476b3f2b5d3b3083de4811fa218dfd56213fcf36
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-09 13:16:07 +0200

    corso service

---
+++ src/main/java/org/vm93/registroelettronico/service/CorsoService.java
@@ -0,0 +1,45 @@
+package org.vm93.registroelettronico.service;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.vm93.registroelettronico.model.Corso;
+import org.vm93.registroelettronico.repo.CorsoRepository;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class CorsoService {
+
+	@Autowired CorsoRepository repo;
+	@Autowired @Qualifier("fakeCorso") private ObjectProvider<Corso> corsofakeprov;
+	
+	public Page<Corso> getAllCorsi(Pageable pageable) {
+		return (Page<Corso>) repo.findAll(pageable);
+	}
+	
+	public void generaFakeCorso() {
+		saveCorso(corsofakeprov.getObject());
+	}
+	
+	public Corso saveCorso(Corso c) {
+		if (c.getId() != null && repo.existsById(c.getId())) {
+			throw new EntityExistsException("Non puoi salvare un id, utilizza il metodo PUT per modificare");
+		}
+		repo.save(c);
+		return c;
+	}
+	
+	public String deleteCorso(Long id) {
+		if (!repo.existsById(id)) {
+			throw new EntityNotFoundException("L'id inserito non esiste");
+		}
+		repo.deleteById(id);
+		return "Corso eliminato dal DB";
+	}
+	
+}
