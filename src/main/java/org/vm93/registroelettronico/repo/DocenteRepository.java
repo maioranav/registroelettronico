f18b70a2c5b74e5c3cb115153adcdde6c414ec23
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-09 10:24:32 +0200

    clean docente un-used repos

--- src/main/java/org/vm93/registroelettronico/repo/DocenteRepository.java
+++ src/main/java/org/vm93/registroelettronico/repo/DocenteRepository.java
@@ -1,14 +1,11 @@
 package org.vm93.registroelettronico.repo;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.vm93.registroelettronico.model.Docente;
 
 public interface DocenteRepository extends CrudRepository<Docente, Long>, PagingAndSortingRepository<Docente, Long> {
 	
-	public Page<Docente> findById(Object id, Pageable pageable);
 	public boolean existsByEmail(String email);
 	public boolean existsByUsername(String username);
 	public Docente findByEmail(String email);
