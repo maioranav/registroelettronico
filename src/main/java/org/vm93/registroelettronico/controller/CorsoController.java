0d93ed738adfd8e7ec82effdfb8d1c53f9b59259
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-09 13:16:18 +0200

    get delete corso controller

---
+++ src/main/java/org/vm93/registroelettronico/controller/CorsoController.java
@@ -0,0 +1,35 @@
+package org.vm93.registroelettronico.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.vm93.registroelettronico.service.CorsoService;
+
+@CrossOrigin(origins =  "*", maxAge = 360000)
+@RestController
+@RequestMapping("/api/corsi")
+public class CorsoController {
+	
+	@Autowired CorsoService service;
+	
+	@GetMapping
+	@PreAuthorize("isAuthenticated()")
+	public ResponseEntity<?> getAll(Pageable pageable) {
+		return new ResponseEntity<>(service.getAllCorsi(pageable), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	@PreAuthorize("isAuthenticated()")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		return new ResponseEntity<String>(service.deleteCorso(id), HttpStatus.OK);
+	}
+
+}
